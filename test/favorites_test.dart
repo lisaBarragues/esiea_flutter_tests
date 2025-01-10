46c75108ad9c233a9b380e5eae254f520a880d39
Edouard Marquez <moi@edouard-marquez.me>
2025-01-10 12:19:01 +0100

    Tests unitaires

---
+++ test/favorites_test.dart
@@ -0,0 +1,22 @@
+import 'package:test/test.dart';
+import 'package:testing_app/models/favorites.dart';
+
+void main() {
+  group('Testing App Provider', () {
+    Favorites favorites = Favorites();
+
+    test('A new item should be added', () {
+      int number = 35;
+      favorites.add(number);
+      expect(favorites.items.contains(number), true);
+    });
+
+    test('An item should be removed', () {
+      int number = 45;
+      favorites.add(number);
+      expect(favorites.items.contains(number), true);
+      favorites.remove(number);
+      expect(favorites.items.contains(number), false);
+    });
+  });
+}
